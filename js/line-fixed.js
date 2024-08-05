262efb0e3016f366d25371f93f5a558cf8eb9757
Vincent Li <v6152445@gmail.com>
2024-08-05 09:38:35 +0800

    Change: 1. NavBar電話改填單按鈕。 2. 手機版置底按鈕只有line

--- js/line-fixed.js
+++ js/line-fixed.js
@@ -1,12 +1,15 @@
-customElements.define('line-fixed', class extends HTMLElement {
+customElements.define(
+  "line-fixed",
+  class extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = `
+      this.innerHTML = `
             <a href="https://lin.ee/jrJI6RD" target="_blank" class="line-fixed" id="line-fixed">
-                <span>專家預約</span>
+                <span>線上客服預約</span>
                 <div class="img-container">
                     <img src="../image/other/line_icon_onbtn.svg" alt="line_icon_onbtn.svg">
                 </div>
             </a>
         `;
     }
-});
+  }
+);
