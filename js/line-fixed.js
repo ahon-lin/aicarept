cc5baba6e76c7da3f3bb383d47d99b3ba293d45b
ahon <ahon.lin@mecotech.com.tw>
2025-11-21 16:37:17 +0800

    change 增加地域分院名稱

--- js/line-fixed.js
+++ js/line-fixed.js
@@ -6,7 +6,7 @@ customElements.define(
             <a href="https://lin.ee/jrJI6RD" target="_blank" class="line-fixed" id="line-fixed">
                 <span>線上客服預約</span>
                 <div class="img-container">
-                    <img src="../image/other/line_icon_onbtn.svg" alt="line_icon_onbtn.svg">
+                    <img src="/image/other/line_icon_onbtn.svg" alt="line_icon_onbtn.svg">
                 </div>
             </a>
         `;
